cb9b14b95a1a507f65f3dc4c8497945c86d01a02
Vitor <moutimg@gmail.com>
2022-05-06 15:15:45 -0300

    feat: develop challenge 2

---
+++ desafio2.sql
@@ -0,0 +1,4 @@
+SELECT COUNT(music_id) AS 'cancoes',
+(SELECT COUNT(artist_id) FROM SpotifyClone.artist) AS 'artistas',
+(SELECT COUNT(album_id) FROM SpotifyClone.album) AS 'albuns'
+FROM SpotifyClone.music;
